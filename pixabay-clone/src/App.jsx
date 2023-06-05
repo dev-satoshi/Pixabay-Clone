fea01fe5f13400d76a8d6b39b715cefe14402d49
satoshi <dev.satoshi.1016@gmail.com>
2023-06-05 12:09:58 +0900

    add: メイン機能の実装

--- pixabay-clone/src/App.jsx
+++ pixabay-clone/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react'
+import './css/App.css'
+import ImageGallery from './component/ImageGallery'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ fetchData, setFetchData ] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    // APIURL
+    const endpointURL = `https://pixabay.com/api/?key=37034299-8f22dd2e0bff86e6bfed89e11&q=${ref.current.value}&image_type=photo`;
+    // APIを叩く(データフェッチング)
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setFetchData(data.hits);
+      })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <h2>Pixabay Clone</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='画像を探す' ref={ref} />
+      </form>
+      <ImageGallery fetchData={fetchData} />
+    </div>
   )
 }
 
